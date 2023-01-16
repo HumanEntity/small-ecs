508cd3831acf41ebcc0e4c65c142df1cd8b8b029
HumanEntity <human_entity@icloud.com>
2023-01-16 20:47:29 +0100

    Little benchmark change

--- benches/hundred_entities.rs
+++ benches/hundred_entities.rs
@@ -2,11 +2,11 @@ use comps::SimpleComponent;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use smecs::prelude::*;
 
-mod comps{
+mod comps {
     use crate::*;
 
     #[derive(Debug)]
-    pub struct SimpleComponent{
+    pub struct SimpleComponent {
         pub foo: u8,
     }
     impl Component for SimpleComponent {
@@ -28,15 +28,29 @@ fn hundred_entities_benchmark(c: &mut Criterion) {
             }
         })
     })
-    .bench_function("hundred_entities_updating_benchmark", |b| b.iter(|| {
-        let mut container = black_box(Container::new());
-        for _ in 0..100 {
-            let mut entity = Entity::new();
-            entity.add_component(SimpleComponent{foo: 0});
-            container.add(entity);
-        }
-        container.update();
-    }));
+    .bench_function("lot of entities adding", |b| {
+        b.iter(|| {
+            let mut container = black_box(Container::new());
+            for _ in 0..1_000 {
+                let mut entity = Entity::new();
+                entity.add_component(SimpleComponent { foo: 0 });
+                container.add(entity);
+            }
+            container.start();
+            container.update();
+        })
+    })
+    .bench_function("hundred_entities_updating_benchmark", |b| {
+        b.iter(|| {
+            let mut container = black_box(Container::new());
+            for _ in 0..100 {
+                let mut entity = Entity::new();
+                entity.add_component(SimpleComponent { foo: 0 });
+                container.add(entity);
+            }
+            container.update();
+        })
+    });
 }
 
 criterion_group!(benches, hundred_entities_benchmark);
